a17b1b1654be42c5ec736f329f7b07b557278bed
i-got-to-eat <khaitamvo@gmail.com>
2026-01-19 20:15:31 -0800

    feat: chapter 9

---
+++ chapter 9/9.1.1_Statements.cpp
@@ -0,0 +1,43 @@
+// g++ -std=c++17 6.1.1_TypeDeclarations.cpp
+
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstring>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <future>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <tuple>
+#include <unordered_map>
+#include <utility>
+#include <valarray>
+#include <vector>
+using namespace std;
+/*
+ Declare Variables
+  - within the smallest scope possible
+  
+ Loops
+  - for
+  - while
+  - do while
+  - range-based for
+  - goto
+
+  
+*/
